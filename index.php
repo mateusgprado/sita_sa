9fddbaba054b2b44ebd32132a5ac8c36e5c3ae56
mateus_g_prado <mateus_g_prado@10.1.105.58>
2017-10-27 08:29:01 -0200

    Versão inicial do projeto

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/estilo.css" type="text/css">
+        <title>Digital Tech</title>
+    </head>
+    <body>
+        <div id="container">
+            <header>
+                <section>
+                    <nav id="menu">
+                        <ul>
+                            <li>
+                                <a href="index.html">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="produtos.html">
+                                    Produtos
+                                </a>
+                            </li>
+                            <li>
+                                <a href="serviços.html">
+                                    Serviços
+                                </a>
+                            </li>
+                            <li>
+                                <a href="colaboradores.html">
+                                    Colaboradores
+                                </a>
+                            </li>
+                            <img src="img/logo.jpg" alt="logo" ID="logo"/>
+                        </ul>
+                    </nav>
+                </section>
+            </header>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
