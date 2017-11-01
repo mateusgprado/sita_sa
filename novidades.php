3e590da43b13b01054b0f25476c1d91aba3245c4
daniela_f_silva <daniela_f_silva@SNRSL1ELBIET049>
2017-11-01 10:53:44 -0200

    Atualizaçao

--- novidades.php
+++ novidades.php
@@ -4,4 +4,37 @@
         <li><h2 class="cinza">DESTAQUES</h2></li>
     </ul>
 </div>
+<div id="album">
+    <div class="item">
+        <img src="img/1.jpg" alt='Foto1' class="fotos">
+        <div class="desc">Apple Macbook</div>
+        <div class="preco">R$ 3999,30</div>
+    </div>
+    <div class="item">
+        <img src="img/9.jpg" alt='Foto9' class="fotos">
+        <div class="desc">Placa Mae Gigabyte </div>
+        <div class="preco">R$ 370,00</div>
+    </div>    
+    <div class="item">
+        <img src="img/3.png" alt='Foto3' class="fotos">
+        <div class="desc">Processador Intel Core i5-7400 </div>
+        <div class="preco">  R$ 700,00</div>
+    </div>    
+    <div class="item">
+        <img src="img/20.jpg" alt='Foto20' class="fotos">
+        <div class="desc">Memória Intel </div>
+        <div class="preco"> R$ 214,00</div>
+    </div>    
+    <div class="item">
+        <img src="img/7.jpg" alt='Foto7' class="fotos">
+        <div class="desc">iPad 32GB</div>
+        <div class="preco"> R$1.750,50</div>
+    </div>    
+    <div class="item">
+        <img src="img/5.jpg" alt='Foto5' class="fotos">
+        <div class="desc">iPad 32GB</div>
+        <div class="preco"> R$1.750,50</div>
+    </div>    
+    
+</div>
 
