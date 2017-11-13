74e1c03bcf0cb4b446a16c68c6191412cda7e4f0
daniela_f_silva <daniela_f_silva@SNRSL1ELBIET049>
2017-11-13 09:45:07 -0200

    site atualizado

--- index.php
+++ index.php
@@ -4,21 +4,50 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css/estilo.css" type="text/css">
         <title>Digital Tech</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+        <script>
+            //paste this code under head tag or in a seperate js file.
+            // Wait for window load
+            $(window).load(function () {
+                // Animate loader off screen
+                $(".se-pre-con").delay(1000).fadeOut("slow");
+                ;
+            });
+        </script>
     </head>
     <body>
+        <div class="se-pre-con"></div>        
         <div class="fundo">
-            <div id="conteudo">
-                <div id="intro">
-                    <iframe id="myFrame" src="home.php"></iframe>
-                    <p id="demo"></p> 
-                    <script>
-                        document.getElementById("myFrame").addEventListener("load", myFunction);
-
-                        function myFunction() {
-                            document.getElementById("demo").innerHTML = "Iframe is loaded.";
-                        }
-                    </script>
+            <div id="container">
+                <header> 
+                    <?php
+                    include "menu.php";
+                    ?>
+                </header>
+                <br/>
+                <section class="titulo">
+                    <h1><p>BEM VINDO AO NOSSO SITE!</p></h1>
+                </section>
+                <div id="banner">
+                    <?php
+                    include 'banner.php';
+                    ?>
                 </div>
+                <div id="galeria">
+                    <?php
+                    include 'novidades.php';
+                    ?>
+                </div>
+                <div id="promacao">
+                    <?php
+                    include 'promocao.php';
+                    ?>
+                </div>
+                <br/>
+                <footer> 
+                    <p> Apendizagem em informática -2017 &copy; todos os direitos reservados. </p>
+                </footer>  
             </div>
         </div>
     </body>
